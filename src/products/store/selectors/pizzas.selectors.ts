eb62150f45c897789f151348253ea49d7c08c696
Oldřich Todt <todt.oldrich@outlook.com>
2022-11-21 20:46:54 +0100

    Add toppings and selected toppings store with pizza-visualised selector

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -3,6 +3,7 @@ import { createSelector } from "@ngrx/store";
 import * as fromRoot from "../../../app/store";
 import * as fromFeature from "../reducers";
 import * as fromPizzas from "../reducers/pizzas.reducer";
+import * as fromToppings from "./toppings.selectors";
 
 import { Pizza } from "../../models/pizza.model";
 
@@ -35,4 +36,18 @@ export const getPizzasLoaded = createSelector(
 export const getPizzaLoading = createSelector(
   getPizzaState,
   fromPizzas.getPizzasLoading
+);
+
+export const getPizzaVisualised = createSelector(
+  getSelectedPizza,
+  fromToppings.getToppingEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingEntities, selectedToppings) => {
+    const toppings = selectedToppings.map((id) => toppingEntities[id]);
+
+    return {
+      ...pizza,
+      toppings,
+    };
+  }
 );
